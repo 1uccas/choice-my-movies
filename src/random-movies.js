2b5d7d2b1ccaec79dbc2dd41b336aee3d884c08c
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-08 17:01:46 -0300

    feat(js): add new function sortMovies()

--- src/random-movies.js
+++ src/random-movies.js
@@ -14,6 +14,24 @@ fetch(json)
   })
   .then(data => {
     console.log(data);
+
+    let selectFilms = [];
+    let maxArray = 50//data.length;
+
+    function sortMovies() {
+        if (selectFilms.length == maxArray) {
+            if (confirm('You have already exceeded the limit! Do you want to start over?')) selectFilms = [];
+            else return;
+        }
+        let sortNumber = Math.ceil(Math.random() * maxArray); // Escolher um numero ao acaso
+        while (selectFilms.indexOf(sortNumber) >= 0) {  // Enquanto o numero já existir, escolher outro
+          sortNumber = Math.ceil(Math.random() * maxArray);
+        }
+        selectFilms.push(sortNumber); // adicionar este numero à array de numeros sorteados para futura referência
+        return sortNumber; // devolver o numero único
+    }
+
+    console.log(sortMovies());
   })
   .catch(error => {
     console.error('Erro ao ler o arquivo JSON:', error);
